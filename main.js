b95edac7f69e729e236916d77d5bde08c8aee6a5
QuintinForsyth1 <22027564@saultcollege.ca>
2024-11-06 14:46:26 -0500

    bunch of errors right now

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import createButton from './buttonScript.js';
 export function main(){
     createButton("Play",() => {
         console.log("Going to weather app!")
-        window.location.replace('openWeather.html');
+        window.location.replace('openweather.html');
     });
     
 }
